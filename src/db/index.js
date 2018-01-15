a35d68058dd23f934dc39bdcca08c2aacaf86d44
Alban Depretz <alband@bam.tech>
2018-01-15 20:46:36 +0100

    Transcrypt credentials

--- src/db/index.js
+++
@@ -1,9 +0,0 @@
-import AWS from "aws-sdk";
-import credentials from "../config/credentials";
-
-AWS.config.update({
-  credentials,
-  endpoint: "http://localhost:8000"
-});
-
-export default AWS.DynamoDB();
