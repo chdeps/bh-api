07d04cf0b353ee61e2dfe92619b19c4901a0110a
Alban Depretz <alband@bam.tech>
2018-03-26 23:43:19 +0200

    Staging env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,17 @@ module.exports = {
         script: './src/index.js'
     }],
     deploy: {
-        production: {
+        staging: {
             user: 'ubuntu',
             host: 'ec2-13-59-3-213.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/bh-api.pem',
-            ref: 'origin/dev',
+            ref: 'origin/feature/setupPM2',
             repo: 'git@github.com:chdeps/bh-api.git',
             path: '/home/ubuntu',
-            'post-deploy': 'yarn && pm2 startOrRestart ecosystem.config.js'
+            'post-deploy': 'npm install && npm run restart'
         }
     }
 }
+
+
+
