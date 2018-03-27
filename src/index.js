57115a27842c4043bfddc49be708f69f429e2210
Alban Depretz <alband@bam.tech>
2018-03-27 23:18:35 +0200

    Remove public index.html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import express from "express";
-
-const app = express();
-//GRAPHQL setup
-/*
 import bodyParser from "body-parser";
 import { graphqlExpress } from "apollo-server-express";
 import schema from "./schema";
-app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
-*/
 
-app.use(express.static('public'));
+const app = express();
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 
 const PORT = 3000;
 app.listen(PORT, () => {
